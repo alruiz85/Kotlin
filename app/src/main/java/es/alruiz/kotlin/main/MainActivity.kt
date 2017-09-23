44d9ccf777029af9264d95a3b63b760958740760
Alfonso Ruiz <alruiz85@gmail.com>
2017-09-23 23:14:42 +0200

    more code

---
+++ app/src/main/java/es/alruiz/kotlin/main/MainActivity.kt
@@ -0,0 +1,44 @@
+package es.alruiz.kotlin.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import es.alruiz.kotlin.R
+import es.alruiz.kotlin.recycler.RecyclerActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG: String = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        message.text = "Hello Kotlin!!!"
+
+        //OnClickListener
+        btn_main_recycler.setOnClickListener {
+            val intent = Intent(this, RecyclerActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        //Toast
+        toast("Toas1")
+        //niceToast("Toast2", TAG, 0)
+    }
+
+    /**
+     * Toast example.
+     */
+    fun niceToast(message: String,
+                  tag: String = MainActivity::class.java.simpleName,
+                  length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, "[$tag] $message", length).show()
+    }
+
+
+}
